8eb921502d1479ebb57f73677d95dcdb7a96fdca
Hans Civrny <jiripokornypraha3@seznam.cz>
2025-06-01 16:03:21 +0200

    Upgraded mobile compatability

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,129 +5,152 @@ import "./formHandler";
 /*console.log("formHandler.js script loaded successfully");*/
 
 export const Footer: React.FC = () => {
+    const isMobile = /Mobi|Android/i.test(navigator.userAgent);
+
     return (
         <footer className="bg-[#333652] text-white py-16 px-4">
             <div className="container mx-auto max-w-6xl">
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-12 mb-12">
-                    <div>
-                        <h3 className="text-2xl font-bold mb-4">O Autorovi</h3>
-                        <div className="flex items-start mb-4">
-                            <div className="mr-4 bg-gray-300 rounded-full h-16 w-16 flex items-center justify-center">
-                                <User size={32} className="text-[#333652]" />
+                {isMobile ? (
+                    <div className="text-center">
+                        <button
+                            onClick={() => {
+                                window.location.href =
+                                    "https://docs.google.com/forms/d/e/1FAIpQLSfaRKuri7CjdnIDuGeAdUHMd3CJUAJr919jTuMMceTTMoYcDA/viewform?usp=dialog";
+                            }}
+                            className="bg-[#fad02c] hover:bg-opacity-90 text-[#333652] font-bold py-3 px-6 rounded-lg transition-all"
+                        >
+                            Napsat zprávu
+                        </button>
+                    </div>
+                ) : (
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-12 mb-12">
+                        <div>
+                            <h3 className="text-2xl font-bold mb-4">
+                                O Autorovi
+                            </h3>
+                            <div className="flex items-start mb-4">
+                                <div className="mr-4 bg-gray-300 rounded-full h-16 w-16 flex items-center justify-center">
+                                    <User
+                                        size={32}
+                                        className="text-[#333652]"
+                                    />
+                                </div>
+                                <div>
+                                    <h4 className="font-bold text-[#fad02c] mb-2">
+                                        Jan Čivrný
+                                    </h4>
+                                    <p className="text-gray-300 mb-4">
+                                        Výzkumník na vlastní pěst, student.
+                                        Zajímám se o fyziku, biologii a chemii.
+                                        Věnuji se výzkumu pasivních chladicích
+                                        barev a jejich aplikacím v boji proti
+                                        klimatickým změnám.
+                                        <br />
+                                        Zkušenosti jsem nabíral v práci na{" "}
+                                        <span className="font-bold">
+                                            UFE a UChP AV.
+                                        </span>
+                                    </p>
+                                </div>
                             </div>
-                            <div>
+
+                            <div className="mt-6">
                                 <h4 className="font-bold text-[#fad02c] mb-2">
-                                    Jan Čivrný
+                                    Poděkování
                                 </h4>
-                                <p className="text-gray-300 mb-4">
-                                    Výzkumník na vlastní pěst, student. Zajímám
-                                    se o fyziku, biologii a chemii. Věnuji se
-                                    výzkumu pasivních chladicích barev a jejich
-                                    aplikacím v boji proti klimatickým změnám.
-                                    <br />
-                                    Zkušenosti jsem nabíral v práci na{" "}
+                                <p className="text-gray-300">
+                                    Především bych chtěl poděkovat mému
+                                    konzultantovi{" "}
                                     <span className="font-bold">
-                                        UFE a UChP AV.
-                                    </span>
+                                        prof. Ing. Dr. Čestmíru Drašarovi
+                                    </span>{" "}
+                                    z FChT UPa. za jeho čas, trpělivost, sdílení
+                                    zkušeností, obětavou pomoc a odborné
+                                    konzultace. Nejvíce si však vážím našeho
+                                    přátelství.
+                                    <br />
+                                    <br /> Děkuji{" "}
+                                    <span className="font-bold">
+                                        prof. Ing. Petře Šulcové Ph.D.
+                                    </span>{" "}
+                                    z FChT UPa za pohotové poskytnutí některých
+                                    bílých pigmentů, které nám scházely.
+                                    <br />
+                                    <br /> Děkuji{" "}
+                                    <span className="font-bold">
+                                        Lučebním závodům Kolín, a.s.
+                                    </span>{" "}
+                                    za bezplatné poskytnutí siloxanového laku
+                                    Lukosil M130. Děkuji také všem ostatním,
+                                    kteří mi pomohli a pomáhají.
                                 </p>
                             </div>
                         </div>
 
-                        <div className="mt-6">
-                            <h4 className="font-bold text-[#fad02c] mb-2">
-                                Poděkování
-                            </h4>
-                            <p className="text-gray-300">
-                                Především bych chtěl poděkovat mému
-                                konzultantovi{" "}
-                                <span className="font-bold">
-                                    prof. Ing. Dr. Čestmíru Drašarovi
-                                </span>{" "}
-                                z FChT UPa. za jeho čas, trpělivost, sdílení
-                                zkušeností, obětavou pomoc a odborné konzultace.
-                                Nejvíce si však vážím našeho přátelství.
-                                <br />
-                                <br /> Děkuji{" "}
-                                <span className="font-bold">
-                                    prof. Ing. Petře Šulcové Ph.D.
-                                </span>{" "}
-                                z FChT UPa za pohotové poskytnutí některých
-                                bílých pigmentů, které nám scházely.
-                                <br />
-                                <br /> Děkuji{" "}
-                                <span className="font-bold">
-                                    Lučebním závodům Kolín, a.s.
-                                </span>{" "}
-                                za bezplatné poskytnutí siloxanového laku
-                                Lukosil M130. Děkuji také všem ostatním, kteří
-                                mi pomohli a pomáhají.
-                            </p>
+                        <div>
+                            <h3 className="text-2xl font-bold mb-6">
+                                Kontaktujte Nás
+                            </h3>
+                            <form className="space-y-4" id="contactForm">
+                                <div>
+                                    <label
+                                        htmlFor="name"
+                                        className="block text-sm font-medium text-gray-300 mb-1"
+                                    >
+                                        Jméno
+                                    </label>
+                                    <input
+                                        type="text"
+                                        id="name"
+                                        name="Name"
+                                        className="w-full px-4 py-2 rounded-lg bg-white/10 border border-white/20 focus:border-[#fad02c] focus:outline-none text-white"
+                                        required
+                                    />
+                                </div>
+
+                                <div>
+                                    <label
+                                        htmlFor="email"
+                                        className="block text-sm font-medium text-gray-300 mb-1"
+                                    >
+                                        Email
+                                    </label>
+                                    <input
+                                        type="email"
+                                        id="email"
+                                        name="Email"
+                                        className="w-full px-4 py-2 rounded-lg bg-white/10 border border-white/20 focus:border-[#fad02c] focus:outline-none text-white"
+                                        required
+                                    />
+                                </div>
+
+                                <div>
+                                    <label
+                                        htmlFor="message"
+                                        className="block text-sm font-medium text-gray-300 mb-1"
+                                    >
+                                        Zpráva
+                                    </label>
+                                    <textarea
+                                        id="message"
+                                        name="Message"
+                                        rows={4}
+                                        className="w-full px-4 py-2 rounded-lg bg-white/10 border border-white/20 focus:border-[#fad02c] focus:outline-none text-white"
+                                        required
+                                    ></textarea>
+                                </div>
+
+                                <button
+                                    type="submit"
+                                    className="bg-gradient-to-r from-[#fad02c] to-[#ff5733] text-[#333652] font-bold py-3 px-6 rounded-lg transition-all duration-500 flex items-center hover:bg-gradient-to-r hover:from-[#ff5733] hover:to-[#3357ff] hover:text-white focus:outline-none focus:ring-2 focus:ring-[#fad02c] focus:ring-offset-2 shadow-md subtle-button"
+                                >
+                                    <Mail className="mr-2" size={20} />
+                                    Odeslat zprávu
+                                </button>
+                            </form>
                         </div>
                     </div>
-
-                    <div>
-                        <h3 className="text-2xl font-bold mb-6">
-                            Kontaktujte Nás
-                        </h3>
-                        <form className="space-y-4" id="contactForm">
-                            <div>
-                                <label
-                                    htmlFor="name"
-                                    className="block text-sm font-medium text-gray-300 mb-1"
-                                >
-                                    Jméno
-                                </label>
-                                <input
-                                    type="text"
-                                    id="name"
-                                    name="Name"
-                                    className="w-full px-4 py-2 rounded-lg bg-white/10 border border-white/20 focus:border-[#fad02c] focus:outline-none text-white"
-                                    required
-                                />
-                            </div>
-
-                            <div>
-                                <label
-                                    htmlFor="email"
-                                    className="block text-sm font-medium text-gray-300 mb-1"
-                                >
-                                    Email
-                                </label>
-                                <input
-                                    type="email"
-                                    id="email"
-                                    name="Email"
-                                    className="w-full px-4 py-2 rounded-lg bg-white/10 border border-white/20 focus:border-[#fad02c] focus:outline-none text-white"
-                                    required
-                                />
-                            </div>
-
-                            <div>
-                                <label
-                                    htmlFor="message"
-                                    className="block text-sm font-medium text-gray-300 mb-1"
-                                >
-                                    Zpráva
-                                </label>
-                                <textarea
-                                    id="message"
-                                    name="Message"
-                                    rows={4}
-                                    className="w-full px-4 py-2 rounded-lg bg-white/10 border border-white/20 focus:border-[#fad02c] focus:outline-none text-white"
-                                    required
-                                ></textarea>
-                            </div>
-
-                            <button
-                                type="submit"
-                                className="bg-gradient-to-r from-[#fad02c] to-[#ff5733] text-[#333652] font-bold py-3 px-6 rounded-lg transition-all duration-500 flex items-center hover:bg-gradient-to-r hover:from-[#ff5733] hover:to-[#3357ff] hover:text-white focus:outline-none focus:ring-2 focus:ring-[#fad02c] focus:ring-offset-2 shadow-md subtle-button"
-                            >
-                                <Mail className="mr-2" size={20} />
-                                Odeslat zprávu
-                            </button>
-                        </form>
-                    </div>
-                </div>
+                )}
 
                 <div className="pt-8 border-t border-white/20 text-center text-gray-400">
                     <p className="max-w-2xl mx-auto px-4 text-sm">
--- src/components/formHandler.js
+++ src/components/formHandler.js
@@ -1,63 +1,80 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Check if the user is on a mobile device
+    const isMobile = /Mobi|Android/i.test(navigator.userAgent);
+
     const form = document.getElementById("contactForm");
-    const submitButton = form.querySelector("button[type='submit']");
+    const submitButton = form?.querySelector("button[type='submit']");
 
-    form.addEventListener("submit", async function (e) {
-        e.preventDefault();
+    if (isMobile && submitButton) {
+        // Change button color to red on mobile
+        submitButton.style.backgroundColor = "red";
 
-        // Add a loading animation to the button
-        submitButton.disabled = true;
-        submitButton.style.background =
-            "linear-gradient(90deg, #fad02c, #ff5733, #33ff57, #3357ff)";
-        submitButton.style.backgroundSize = "400% 400%";
-        submitButton.style.animation = "gradientAnimation 2s ease infinite";
+        // Add a click event to redirect to Google Form
+        submitButton.addEventListener("click", (e) => {
+            e.preventDefault(); // Prevent default form submission
+            window.location.href =
+                "https://docs.google.com/forms/d/e/1FAIpQLSfaRKuri7CjdnIDuGeAdUHMd3CJUAJr919jTuMMceTTMoYcDA/viewform?usp=dialog";
+        });
+    }
 
-        // Collect form data
-        const name = form.elements["Name"].value;
-        const email = form.elements["Email"].value;
-        const message = form.elements["Message"].value;
+    if (!isMobile && form) {
+        form.addEventListener("submit", async function (e) {
+            e.preventDefault();
 
-        const data = {
-            fields: {
-                Name: name, // Airtable 'Name' column
-                Email: email, // Airtable 'Email' column
-                Message: message, // Airtable 'Message' column
-            },
-        };
+            // Add a loading animation to the button
+            submitButton.disabled = true;
+            submitButton.style.background =
+                "linear-gradient(90deg, #fad02c, #ff5733, #33ff57, #3357ff)";
+            submitButton.style.backgroundSize = "400% 400%";
+            submitButton.style.animation = "gradientAnimation 2s ease infinite";
 
-        // Airtable API details
-        const url = "https://api.airtable.com/v0/appKD0fy2vfEwWKvM/TABLE";
+            // Collect form data
+            const name = form.elements["Name"].value;
+            const email = form.elements["Email"].value;
+            const message = form.elements["Message"].value;
 
-        try {
-            const response = await fetch(url, {
-                method: "POST",
-                headers: {
-                    Authorization:
-                        "Bearer patX654RwXfgBsOeQ.e44033edd248b8408904551ee93414ef0dce30a41ae2668c49e9496b96f1832c",
-                    "Content-Type": "application/json",
+            const data = {
+                fields: {
+                    Name: name, // Airtable 'Name' column
+                    Email: email, // Airtable 'Email' column
+                    Message: message, // Airtable 'Message' column
                 },
-                body: JSON.stringify(data),
-            });
+            };
 
-            if (response.ok) {
-                alert("Zpráva byla úspěšně odeslána!");
-                form.reset();
-            } else {
-                const result = await response.json();
-                alert(
-                    "Chyba při odesílání: " +
-                        (result.error?.message || "Neznámá chyba")
-                );
+            // Airtable API details
+            const url = "https://api.airtable.com/v0/appKD0fy2vfEwWKvM/TABLE";
+
+            try {
+                const response = await fetch(url, {
+                    method: "POST",
+                    headers: {
+                        Authorization:
+                            "Bearer patX654RwXfgBsOeQ.e44033edd248b8408904551ee93414ef0dce30a41ae2668c49e9496b96f1832c",
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(data),
+                });
+
+                if (response.ok) {
+                    alert("Zpráva byla úspěšně odeslána!");
+                    form.reset();
+                } else {
+                    const result = await response.json();
+                    alert(
+                        "Chyba při odesílání: " +
+                            (result.error?.message || "Neznámá chyba")
+                    );
+                }
+            } catch (error) {
+                alert("Chyba při odesílání: " + error.message);
+            } finally {
+                // Remove the loading animation and re-enable the button
+                submitButton.disabled = false;
+                submitButton.style.background = "";
+                submitButton.style.animation = "";
             }
-        } catch (error) {
-            alert("Chyba při odesílání: " + error.message);
-        } finally {
-            // Remove the loading animation and re-enable the button
-            submitButton.disabled = false;
-            submitButton.style.background = "";
-            submitButton.style.animation = "";
-        }
-    });
+        });
+    }
 
     // Add keyframes for the gradient animation
     const style = document.createElement("style");
